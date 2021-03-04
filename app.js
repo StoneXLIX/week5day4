214a9d783bbfbd9c3f2c41a6881849658afeeeb9
Stone <stonexlix@gmail.com>
2021-03-03 19:16:45 -0700

    Finished the project

---
+++ app.js
@@ -0,0 +1,16 @@
+let a = $(".navItem");
+$(a).css('color', 'white');
+
+let b = $('#nav');
+$(b).css('background-color', '#145');
+
+let c = $('ul li').even();
+$(c).css('background-color', '#ccc');
+
+let d = $('ul li').odd();
+$(d).css('background-color', '#aaa');
+
+let e = $('input');
+$(e).val('Stone');
+
+
